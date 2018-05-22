72760b12ddae1625158a5c1118d8e60b05e258dc
Chatura Ahangama <chaturadroid@gmail.com>
2018-05-22 13:44:38 -0700

    Update

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var createRow = function(data) {
+    // Get reference to existing tbody element, create a new table row element
+    var tBody = $("tbody");
+    var tRow = $("<tr>");
+
+    // Methods run on jQuery selectors return the selector they we run on
+    // This is why we can create and save a reference to a td in the same statement we update its text
+    var titleTd = $("<td>").text(data.Title);
+    var yearTd = $("<td>").text(data.Year);
+    var actorsTd = $("<td>").text(data.Actors);
+    // Append the newly created table data to the table row
+    tRow.append(titleTd, yearTd, actorsTd);
+    // Append the table row to the table body
+    tBody.append(tRow);
+  };
